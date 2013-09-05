310ea6c25b5999f5626ba13f1c1179c87855f4fa
hiroki <allen.vijf@gmail.com>
2013-09-05 10:55:05 +0900

    Add .gitignore

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git-learn
+//
+//  Created by hiroki on 2013/09/05.
+//  Copyright (c) 2013年 team5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  git-learn
+//
+//  Created by hiroki on 2013/09/05.
+//  Copyright (c) 2013年 team5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
